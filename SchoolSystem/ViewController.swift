ca2b3c4683232c5f809e8ddd4e89910b2e98af60
muryno <murainoy@yahoo.com>
2020-01-23 11:09:02 +0100

    Initial Commit

---
+++ SchoolSystem/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SchoolSystem
+//
+//  Created by Muraino Yakubu on 1/23/20.
+//  Copyright © 2020 Muraino Yakubu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
